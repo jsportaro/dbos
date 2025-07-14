34fd494613de4df3e029411a25fdd5fba2a4613b
Joe Portaro <jsportaro@outlook.com>
2025-07-14 13:31:04 -0400

    Added linker script and more robust build script

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -63,17 +63,68 @@ gdt_descriptor:
     dw gdt_end - gdt_start - 1
     dd gdt_start
 
-BITS 32
+[BITS 32]
 load32:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov ebp, 0x00200000
-    mov esp, ebp
-    jmp $
+    mov eax, 1          ; Sector to start reading
+    mov ecx, 100        ; Number of sectors to read - Must be less than
+                        ; BINARY_SECTORS in the Makefile
+    mov edi, 0x0100000 ; Must match the linker script - Where we're loading
+                        ; sectors too
+    call ata_lba_read
+    jmp CODE_SEG:0x0100000
+
+ata_lba_read:
+    mov ebx, eax    ; Backup the LBA
+    ; Send the highest 8 bits of the lba to the hard disk controller
+    shr eax, 24
+    or eax, 0xE0 ; Select the  master drive
+    mov dx, 0x1F6
+    out dx, al
+
+    ; Send the total sectors to read
+    mov eax, ecx
+    mov dx, 0x1f2
+    out dx, al
+
+    ; Send more bits to the LBA
+    mov eax, ebx    ; Restore the backup LBA
+    mov dx, 0x1f3
+    out dx, al
+
+    mov eax, ebx   ; Restore the backup LBA
+    mov dx, 0x1f4
+    shr eax, 8
+    out dx, al
+
+    ; Send upper 16 bits of the LBA
+    mov eax, ebx
+    mov dx, 0x1f5
+    shr eax, 16
+    out dx, al
+
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+    ; Read all sectors into memory
+.next_sector:
+    push ecx
+
+; Checking if we need to read
+.try_again:
+    mov dx, 0x1f7
+    in al, dx
+    test al, 8
+    jz .try_again
+
+; We need to read 256 words at a time
+    mov ecx, 256
+    mov dx, 0x1f0
+    rep insw
+    pop ecx
+    loop .next_sector
+
+    ret
 
 times 510-($ - $$) db 0
 dw 0xAA55
